d286f2eba5a1c903e762bc65ee7e4b9ec1d464e8
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-17 23:36:15 +0530

    [Paarth Agarwal] Add. code of reversing a string

---
+++ core-java-practice/leet-code-codebase/reverseString.java
@@ -0,0 +1,25 @@
+public class reverseString {
+    public static String reverseString(String s) {
+        char[] charArray = s.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+
+        while (left < right) {
+            // Swap characters
+            char temp = charArray[left];
+            charArray[left] = charArray[right];
+            charArray[right] = temp;
+
+            left++;
+            right--;
+        }
+
+        return new String(charArray);
+    }
+
+    public static void main(String[] args) {
+        String input = "Hello, World!"; // Example string
+        String result = reverseString(input);
+        System.out.println("Reversed String: " + result);
+    }
+}
