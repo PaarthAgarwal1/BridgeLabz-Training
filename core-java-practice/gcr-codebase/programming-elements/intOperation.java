43dfb41a35454b831dc6e0a6561fedb7fbc0d984
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 16:21:32 +0530

    [Paarth Agarwal] Add. code to perform various integer operations based on user input

---
+++ core-java-practice/gcr-codebase/programming-elements/intOperation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class intOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter first integer:");
+        int firstInt = sc.nextInt();
+        System.err.print("Enter second integer:");
+        int secondInt = sc.nextInt();
+        System.err.print("Enter third integer:");
+        int thirdInt = sc.nextInt();
+
+        int opt1=firstInt + secondInt * thirdInt;
+        int opt2=firstInt * secondInt + thirdInt;
+        int opt3=thirdInt + firstInt / secondInt;
+        int opt4=firstInt % secondInt + thirdInt;
+
+        System.err.println("The result of Int Operations are "+opt1+", "+opt2+", "+opt3+", "+ " and "+opt4);
+    }
+}
