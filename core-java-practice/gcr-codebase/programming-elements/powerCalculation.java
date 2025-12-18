938f1024d0509b5eb9c8605a04a69b1b00c8cd1f
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 10:53:59 +0530

    [Paarth Agarwal] Add. code of power calculation

---
+++ core-java-practice/gcr-codebase/programming-elements/powerCalculation.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class powerCalculation {
+    public static int calculatePower(int base, int exponent) {
+        return (int)Math.pow(base, exponent);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter base (Ex-2) :");
+        int base = sc.nextInt();
+        System.out.println("Enter exponent (Ex-3) :");  
+        int exponent = sc.nextInt();
+        int result = calculatePower(base, exponent); 
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+    }
+}
