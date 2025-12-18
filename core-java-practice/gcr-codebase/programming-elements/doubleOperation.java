d02e00d72d2e31eb230e9c871f32e1f543a7dfed
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 16:21:45 +0530

    [Paarth Agarwal] Add. code to perform various double operations based on user input

---
+++ core-java-practice/gcr-codebase/programming-elements/doubleOperation.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class doubleOperation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.err.print("Enter first number:");
+        double firstNum = sc.nextDouble();
+        System.err.print("Enter second number:");
+        double secondNum = sc.nextDouble();
+        System.err.print("Enter third number:");
+        double thirdNum = sc.nextDouble();
+
+        double opt1=firstNum + secondNum * thirdNum;
+        double opt2=firstNum * secondNum + thirdNum;
+        double opt3=thirdNum + firstNum / secondNum;
+        double opt4=firstNum % secondNum + thirdNum;
+
+        System.err.println("The result of Double Operations are "+opt1+", "+opt2+", "+opt3+", "+ " and "+opt4);
+    }
+    
+}
