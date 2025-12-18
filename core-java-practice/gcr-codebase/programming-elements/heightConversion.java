26ebbada87e24895be49b3513cfee0afd1dc6793
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 14:39:34 +0530

    [Paarth Agarwal] Add. code to convert height from centimeters to feet and inches

---
+++ core-java-practice/gcr-codebase/programming-elements/heightConversion.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class heightConversion {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter height in centimeters (Ex-170.0) :");
+        double heightCm = sc.nextDouble();
+        double heightInches = Math.round(heightCm / 2.54 * 100.0) / 100.0;
+        double heightFeet = Math.round(heightCm / 30.48 * 100.0) / 100.0;
+        System.out.println("Your Height in cm is "+heightCm+"while in feet is "+heightFeet+" and inches is "+heightInches);
+    }
+}
