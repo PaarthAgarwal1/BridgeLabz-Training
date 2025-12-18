b04778146155612b0cdc2aa66903daef7e964358
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 15:22:50 +0530

    [Paarth Agarwal] Add. code to calculate the area of a triangle in square centimeters and square inches

---
+++ core-java-practice/gcr-codebase/programming-elements/areaOfTriangle.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class areaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the base of the triangle (Ex-5.0) :");
+        double base=sc.nextDouble();
+        System.out.print("Enter the height of the triangle (Ex-10.0) :");
+        double height=sc.nextDouble();
+        double areaInches=0.5*base*height*0.394*0.394;
+        double areaCm=0.5*base*height;
+        System.out.println("The area of triangle in square centimeters is "+areaCm+" and in square inches is "+areaInches);
+    }
+}
