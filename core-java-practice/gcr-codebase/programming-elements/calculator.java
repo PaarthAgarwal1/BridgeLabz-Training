c87a45f476f1c66577c597d113566e201e7d3afa
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 15:22:24 +0530

    [Paarth Agarwal] Add. code to perform basic arithmetic operations on two numbers

---
+++ core-java-practice/gcr-codebase/programming-elements/calculator.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers to perform arithmetic operations:");
+        int number1=sc.nextInt();
+        int number2=sc.nextInt();
+
+        int sum = number1 + number2;
+        int difference = number1 - number2;
+        int product = number1 * number2;
+        int quotient = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers "+ number1 +" and "+ number2 +" is "+sum+", "+difference+", "+ product+", "+"and " +quotient);
+    }
+}
