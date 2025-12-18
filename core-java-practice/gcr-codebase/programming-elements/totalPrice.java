42e084d5c383bf93da8ee0216e77cebbd3294b9a
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 15:23:47 +0530

    [Paarth Agarwal] Add. code to calculate the total price based on unit price and quantity

---
+++ core-java-practice/gcr-codebase/programming-elements/totalPrice.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class totalPrice {
+    public static void main (String []args){
+        Scanner sc=new Scanner(System.in);
+        System.err.print("Enter the unit price of the item (Ex-100.0) :");
+        double unitPrice=sc.nextDouble();
+        System.out.print("Enter the quantity of the item (Ex-5) :");
+        int quantity=sc.nextInt();
+        double totalPrice=unitPrice*quantity;
+        System.err.println("The total purchase price is INR "+ totalPrice+" if the quantity "+quantity+" and unit price is INR "+unitPrice);
+    }
+}
