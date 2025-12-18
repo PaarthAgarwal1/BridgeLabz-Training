d6ded760c433999e7777ba81db99fd32568fd137
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-18 10:44:25 +0530

    [Paarth Agarwal] Add. code of calculating simple interest

---
+++ core-java-practice/gcr-codebase/programming-elements/calculateSI.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class calculateSI {
+    public static double calculateSimpleInterest(double principal, double rate, double time) {
+        return Math.round((principal * rate * time) / 100 * 100.0) / 100.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter principal amount (Ex-1000.0) :");
+        double principal = sc.nextDouble();
+        System.out.println("Enter rate of interest (Ex-5.0) :");
+        double rate =   sc.nextDouble(); 
+        System.out.println("Enter time in years (Ex-3.0) :");
+        double time =   sc.nextDouble();     
+        double simpleInterest = calculateSimpleInterest(principal, rate, time);
+        System.out.println("Simple Interest for principal " + principal + ", rate " + rate + "%, over " + time + " years is: " + simpleInterest);
+    }
+}
