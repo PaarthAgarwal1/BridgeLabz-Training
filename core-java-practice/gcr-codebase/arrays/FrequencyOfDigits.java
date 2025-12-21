40e8900041cb8f90d82214d49cab247507c78680
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-21 22:40:42 +0530

    [Paarth Agarwal] Add. Implement the program for calculating the frequency of digits in number

---
+++ core-java-practice/gcr-codebase/arrays/FrequencyOfDigits.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class FrequencyOfDigits {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        int [] frequency = new int[10];
+        //storing the digits of the number in an array
+        while(number>0){
+            int digit=number%10;
+            frequency[digit]++;
+            number=number/10;
+        }
+        //printing the frequency of each digit
+        System.out.println("Digit | Frequency");
+        for(int i=0;i<frequency.length;i++){
+            if(frequency[i]>0){
+                System.out.println("  " + i + "  |    " + frequency[i]);
+            }
+        }
+
+    }
+}
