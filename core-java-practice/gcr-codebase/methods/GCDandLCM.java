32918c6a1c9f7361565a19315528c41f42a3c31e
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-12-24 10:22:27 +0530

    [Paarth Agarwal] Add. Implement the code for gcd and lcm

---
+++ core-java-practice/gcr-codebase/methods/GCDandLCM.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class GCDandLCM {
+
+    // Method to calculate GCD using Euclidean Algorithm
+    public static int calculateGCD(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    // Method to calculate LCM using GCD
+    public static int calculateLCM(int a, int b) {
+        return (a * b) / calculateGCD(a, b);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        // Calculation
+        int gcd = calculateGCD(num1, num2);
+        int lcm = calculateLCM(num1, num2);
+
+        // Output
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+
+        sc.close();
+    }
+}
