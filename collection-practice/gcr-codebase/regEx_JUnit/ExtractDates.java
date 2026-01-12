b5b3c742ce73bcb14fdec290daf8793ddefab2b1
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-01-12 23:44:20 +0530

    [Paarth Agarwal] Add. Implement the code for extracting all the valid dates from the text with the help of regex

---
+++ collection-practice/gcr-codebase/regEx_JUnit/ExtractDates.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the text to extract dates : ");
+        // Taking text input 
+        String text=sc.nextLine();
+        // Regular Expression for dates 
+        String regex="\\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}\\b";
+        // Compiling the regex
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(text);
+        while(matcher.find()){
+            System.out.println("Extracted date : "+ matcher.group());
+        }
+    }
+}
