dbf5d68cd3aaf065f28af4a0eaac08852e792192
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-01-29 02:11:49 +0530

    [Paarth Agarwal] Add. UC12: Sort contacts by city, state or zip

--- java-oops-practice/gcr-codebase/AddressBookSystem/src/main/java/com/addressbook/service/SortService.java
+++ java-oops-practice/gcr-codebase/AddressBookSystem/src/main/java/com/addressbook/service/SortService.java
@@ -21,4 +21,29 @@ public class SortService {
         System.out.println("Contacts sorted by name");
         contacts.forEach(System.out::println);
     }
+    public void sortByField(int choice){
+        List<Contact> contacts=dao.getAllContacts();
+        if(contacts.isEmpty()){
+            System.out.println("Address Book is empty");
+            return;
+        }
+        switch (choice){
+            case 1:
+                contacts.sort(Comparator.comparing(Contact::getCity));
+                System.out.println("Contacts sorted by City : ");
+                break;
+            case 2:
+                contacts.sort(Comparator.comparing(Contact::getState));
+                System.out.println("Contacts sorted by State : ");
+                break;
+            case 3:
+                contacts.sort(Comparator.comparing(Contact::getZip));
+                System.out.println("Contacts sorted by Zip : ");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                return;
+        }
+        contacts.forEach(System.out::println);
+    }
 }
