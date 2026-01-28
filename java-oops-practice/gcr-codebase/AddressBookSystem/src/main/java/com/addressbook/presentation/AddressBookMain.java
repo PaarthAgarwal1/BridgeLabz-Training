9450ba6804d89afe9224398d6e2e3f1afb457e49
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-01-28 22:36:08 +0530

    [Paarth Agarwal] Add. Implement the initial setup and welcome message for addressbook problem

---
+++ java-oops-practice/gcr-codebase/AddressBookSystem/src/main/java/com/addressbook/presentation/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.addressbook.presentation;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
