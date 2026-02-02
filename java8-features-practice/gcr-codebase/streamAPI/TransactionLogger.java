eea1fabc818c5e0edbbbe11d58a5ef854d70836c
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-02-02 23:10:52 +0530

    [Paarth Agarwal] Add. Implement the code for  forEach() to log transaction IDs with timestamps using LocalDateTime

---
+++ java8-features-practice/gcr-codebase/streamAPI/TransactionLogger.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+public class TransactionLogger {
+
+    public static void main(String[] args) {
+
+        // List of transaction IDs
+        List<String> transactionIds=Arrays.asList("TXN1001","TXN1002","TXN1003","TXN1004");
+
+        // Log each transaction with timestamp
+        transactionIds.forEach(id ->System.out.println(LocalDateTime.now() + " - Transaction: " + id));
+    }
+}
