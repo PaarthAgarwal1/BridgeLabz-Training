418362f565fe4daf3aa5c2759ff2c814247140cf
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-02-02 23:37:48 +0530

    [Paarth Agarwal] Add. Implement the full code for smart city transport and service management system

--- java8-features-practice/gcr-codebase/workshop/SmartCityTransportAndServiceManagementSystem.java
+++ java8-features-practice/gcr-codebase/workshop/SmartCityTransportAndServiceManagementSystem.java
@@ -1,8 +1,11 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 interface TransportService{
     String getName();
@@ -98,9 +101,68 @@ class Passenger {
 public class SmartCityTransportAndServiceManagementSystem {
     public static void main(String[] args) {
         List<TransportService> service=Arrays.asList(new BusService(),new MetroService(),new TaxiService(),new AmbulanceService());
+        List<Passenger> passengers=new ArrayList<>();
         Scanner sc=new Scanner(System.in);
-        int choice=sc.nextInt();
-        service.stream().filter(s->!(s is instanceof EmergencyService)).sorted((s1,s2)->)
+
+        while (true) {
+            System.out.println("\n===== SMART CITY DASHBOARD =====");
+            System.out.println("1. View All Services");
+            System.out.println("2. Book a Trip");
+            System.out.println("3. View Emergency Services");
+            System.out.println("4. Revenue Report");
+            System.out.println("0. Exit");
+            System.out.print("Enter choice: ");
+
+            int choice=sc.nextInt();
+            switch(choice){
+                case 1:
+                    service.forEach(TransportService::printServiceDetails);
+                    break;
+                case 2:
+                    System.out.print("Enter Route (A-B / A-C / B-C): ");
+                    String route = sc.next();
+
+                    System.out.print("Peak Time? (true/false): ");
+                    boolean peak = sc.nextBoolean();
+                    List<TransportService> matchServices=service.stream().filter(s->s.getRoute().equalsIgnoreCase(route)).collect(Collectors.toList());
+                    System.out.println("Sort by: 1. Lowest Fare | 2. Earliest Departure");
+                    int pref=sc.nextInt();
+                    if(pref==1){
+                        matchServices.sort(Comparator.comparingDouble(TransportService::getFare));
+                    }else{
+                        matchServices.sort(Comparator.comparingInt(TransportService::getDepartureTime));
+                    }
+                    matchServices.forEach((TransportService::printServiceDetails));
+                    FareCalculator calculator=distance -> distance*10;
+                    System.out.println("Enter start point:");
+                    double start=sc.nextDouble();
+                    System.out.println("Enter end point:");
+                    double end=sc.nextDouble();
+                    double distance = GeoUtils.calculateDistance(start, end);
+                    double totalFare = calculator.calculateFare(distance);
+                    if(peak){
+                        totalFare+=20;
+                    }
+                    passengers.add(new Passenger(route,totalFare,peak));
+                    break;
+                case 3:
+                    service.stream().filter(s->(s instanceof EmergencyService)).forEach(s ->System.out.println("🚑 Priority Service: " + s.getName()));
+                    break;
+                case 4:
+                    passengers.stream().collect(Collectors.groupingBy(p->p.route)).forEach((r,p)->System.out.println(r+" -> "+p.size()));
+                    passengers.stream().collect(Collectors.partitioningBy(p -> p.peakTime)).forEach((k, v) ->System.out.println(k ? "Peak" : "Non-Peak" + " Trips: " + v.size()));
+                    DoubleSummaryStatistics stats=passengers.stream().collect(Collectors.summarizingDouble(p->p.fare));
+                    System.out.println("Total Revenue: "+stats.getSum());
+                    System.out.println("Average Fare: "+stats.getAverage());
+                    break;
+                case 0:
+                    System.out.println("Thanks for using !");
+                    return;
+                default:
+                    System.out.println("Please enter the valid option!");
+            }
+            
+        }
     }
 }
 
