ef22c35b0ff02869245181e3b9753762f302d789
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2026-02-11 15:57:16 +0530

    [Paarth Agarwal] Add. UC14: CSV read and write functionality

--- java-oops-practice/gcr-codebase/AddressBookSystem/src/main/java/com/addressbook/presentation/AddressBookMain.java
+++ java-oops-practice/gcr-codebase/AddressBookSystem/src/main/java/com/addressbook/presentation/AddressBookMain.java
@@ -8,25 +8,26 @@ import com.addressbook.service.AddressBookService;
 import java.util.Scanner;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Program");
-        AddressBookDirectoryService directoryService =
-                new AddressBookDirectoryService();
+   public static void main(String[] args) {
+       System.out.println("Welcome to Address Book Program");
+       AddressBookDirectoryService directoryService =
+               new AddressBookDirectoryService();
 
-        // Create Address Books
-        directoryService.createAddressBook("Personal");
-        directoryService.createAddressBook("Office");
 
-        // Add Contacts
-        directoryService.getAddressBook("Personal").getContacts().add(new Contact("Rahul", "Sharma", "Delhi", "Delhi","Delhi", "110001", "9999999999", "rahul@gmail.com")
-        );
+       // Create Address Books
+       directoryService.createAddressBook("Personal");
+       directoryService.createAddressBook("Office");
 
-        directoryService.getAddressBook("Office").getContacts().add(new Contact("Amit", "Verma", "Delhi", "Delhi","Delhi", "201301", "8888888888", "amit@gmail.com")
-        );
+       // Add Contacts
+       directoryService.getAddressBook("Personal").getContacts().add(new Contact("Rahul", "Sharma", "Delhi", "Delhi","Delhi", "110001", "9999999999", "rahul@gmail.com")
+       );
 
-        // Search
-        directoryService.searchPersonByCity("Delhi");
-        directoryService.searchPersonByCity("UP");
-        directoryService.searchPersonByState("Delhi");
-    }
+       directoryService.getAddressBook("Office").getContacts().add(new Contact("Amit", "Verma", "Delhi", "Delhi","Delhi", "201301", "8888888888", "amit@gmail.com")
+       );
+
+       // Search
+       directoryService.searchPersonByCity("Delhi");
+       directoryService.searchPersonByCity("UP");
+       directoryService.searchPersonByState("Delhi");
+   }
 }
